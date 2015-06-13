b5437f92eb1dcad19ee7d8e137b3d696fedd3891
gustavocgs <cgustavo.88@gmail.com>
2015-06-13 18:24:50 -0300

    Se agregan los SP para la carga de los combos en el script inicial

--- script-sql/script_creacion_inicial.sql
+++ script-sql/script_creacion_inicial.sql
@@ -1594,6 +1594,42 @@ END CATCH
 GO
 
 /***********************
+	Creamos procedures para la carga de los combos en la app
+************************/
+
+CREATE PROCEDURE [SARASA].[cbx_rol]
+AS
+	SELECT Rol_Id 'Valor', Rol_Descripcion 'Etiqueta' FROM SARASA.Rol
+GO
+
+
+CREATE PROCEDURE [SARASA].[cbx_tipodoc]
+AS
+	SELECT Tipodoc_Id 'Valor', Tipodoc_Descripcion 'Etiqueta' FROM SARASA.Tipodoc
+GO
+
+
+CREATE PROCEDURE [SARASA].[cbx_pais]
+AS	
+	SELECT Pais_Id 'Valor', Pais_Nombre 'Etiqueta'
+	FROM SARASA.Pais
+	ORDER BY Pais_Nombre
+GO
+
+
+CREATE PROCEDURE [SARASA].[cbx_moneda]
+AS	
+	SELECT Moneda_Id 'Valor', Moneda_Descripcion 'Etiqueta' 
+	FROM SARASA.Moneda
+GO
+
+
+CREATE PROCEDURE [SARASA].[cbx_tipocta]
+AS	
+	SELECT Tipocta_Id 'Valor', Tipocta_Descripcion 'Etiqueta' 
+	FROM SARASA.Tipocta
+
+/***********************
 	Creamos triggers
 ************************/
 
