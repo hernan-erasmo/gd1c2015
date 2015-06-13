d50a032672d14323f91a1746e0a2153caf377285
Juan Alfonso <juanmalfonso22@gmail.com>
2015-06-13 20:01:05 -0300

    Corrijo un GO del script inicial y agrego los SP que estaban aparte para facturar

--- script-sql/script_creacion_inicial.sql
+++ script-sql/script_creacion_inicial.sql
@@ -1593,6 +1593,126 @@ BEGIN CATCH
 END CATCH
 GO
 
+-- Procedimiento para comprobar si un cliente tiene items sin facturar
+CREATE PROCEDURE SARASA.comprobar_items_impagos (
+	@cliente_id	integer, @comprobante integer OUTPUT
+)
+AS
+
+SET XACT_ABORT ON	-- Si alguna instruccion genera un error en runtime, revierte la transacción.
+SET NOCOUNT ON		-- No actualiza el número de filas afectadas. Mejora performance y reduce carga de red.
+
+DECLARE @starttrancount int
+DECLARE @error_message nvarchar(4000)
+
+
+BEGIN TRY
+	SELECT @starttrancount = @@TRANCOUNT	--@@TRANCOUNT lleva la cuenta de las transacciones abiertas.
+
+	IF @starttrancount = 0
+		BEGIN TRANSACTION
+			
+			IF EXISTS ( SELECT DISTINCT i.Itemfact_Id
+						FROM GD1C2015.SARASA.Itemfact i, GD1C2015.SARASA.Cuenta cu, GD1C2015.SARASA.Cliente c
+						WHERE i.Itemfact_Cuenta_Numero=cu.Cuenta_Numero AND
+						cu.Cuenta_Cliente_Id=@cliente_id AND
+						i.Itemfact_Pagado='0')
+				SET @comprobante='1'
+			ELSE
+				SET @comprobante='0'
+		
+
+	IF @starttrancount = 0
+		COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	IF XACT_STATE() <> 0 AND @starttrancount = 0	--XACT_STATE() es cero sólo cuando no hay ninguna transacción activa para este usuario.
+		ROLLBACK TRANSACTION
+	SELECT @error_message = ERROR_MESSAGE()
+	RAISERROR('Error en la transacción: %s',16,1, @error_message)
+END CATCH
+GO
+
+-- Procedimiento que genera una nueva factura segun cliente
+CREATE PROCEDURE SARASA.facturar_por_cliente (
+	@cliente_id	integer, @factura_id numeric(18,0) OUTPUT
+)
+AS
+
+SET XACT_ABORT ON	-- Si alguna instruccion genera un error en runtime, revierte la transacción.
+SET NOCOUNT ON		-- No actualiza el número de filas afectadas. Mejora performance y reduce carga de red.
+
+DECLARE @starttrancount int
+DECLARE @error_message nvarchar(4000)
+DECLARE @fecha_actual datetime
+SET @fecha_actual = GETDATE()
+
+BEGIN TRY
+	SELECT @starttrancount = @@TRANCOUNT	--@@TRANCOUNT lleva la cuenta de las transacciones abiertas.
+
+	IF @starttrancount = 0
+			BEGIN TRANSACTION
+
+		
+				INSERT INTO SARASA.Factura (Factura_Fecha, Factura_Cliente_Id)
+				VALUES (@fecha_actual, @cliente_id)
+			
+				SET @factura_id = ( SELECT TOP 1 f.Factura_Numero
+								FROM GD1C2015.SARASA.Factura f
+								WHERE f.Factura_Cliente_Id=@cliente_id
+								ORDER BY f.Factura_Fecha ASC )
+		
+
+	IF @starttrancount = 0
+		COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	IF XACT_STATE() <> 0 AND @starttrancount = 0	--XACT_STATE() es cero sólo cuando no hay ninguna transacción activa para este usuario.
+		ROLLBACK TRANSACTION
+	SELECT @error_message = ERROR_MESSAGE()
+	RAISERROR('Error en la transacción: %s',16,1, @error_message)
+END CATCH
+GO
+
+
+-- Procedimiento para facturar los items de una factura
+CREATE PROCEDURE SARASA.facturar_items (
+	@factura_id numeric(18,0), @cliente_id integer
+)
+AS
+
+SET XACT_ABORT ON	-- Si alguna instruccion genera un error en runtime, revierte la transacción.
+SET NOCOUNT ON		-- No actualiza el número de filas afectadas. Mejora performance y reduce carga de red.
+
+DECLARE @starttrancount int
+DECLARE @error_message nvarchar(4000)
+
+BEGIN TRY
+	SELECT @starttrancount = @@TRANCOUNT	--@@TRANCOUNT lleva la cuenta de las transacciones abiertas.
+
+	IF @starttrancount = 0
+		BEGIN TRANSACTION
+
+		UPDATE GD1C2015.SARASA.Itemfact
+		SET Itemfact_Factura_Numero=@factura_id,  Itemfact_Pagado='1'
+		WHERE Itemfact_Pagado='0' AND
+				Itemfact_Id IN (SELECT DISTINCT i.Itemfact_Id
+								FROM GD1C2015.SARASA.Itemfact i, GD1C2015.SARASA.Cuenta cu, GD1C2015.SARASA.Cliente c
+								WHERE i.Itemfact_Cuenta_Numero=cu.Cuenta_Numero AND
+										cu.Cuenta_Cliente_Id=@cliente_id)
+
+	IF @starttrancount = 0
+		COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+	IF XACT_STATE() <> 0 AND @starttrancount = 0	--XACT_STATE() es cero sólo cuando no hay ninguna transacción activa para este usuario.
+		ROLLBACK TRANSACTION
+	SELECT @error_message = ERROR_MESSAGE()
+	RAISERROR('Error en la transacción: %s',16,1, @error_message)
+END CATCH
+GO
+
+
 /***********************
 	Creamos procedures para la carga de los combos en la app
 ************************/
