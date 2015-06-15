a9d3220aa9dd7daf3299f20aab1d21a9adcf89b2
Juan Alfonso <juanmalfonso22@gmail.com>
2015-06-15 19:03:31 -0300

    Corrijo el usuario por default para iniciar sesion, admin

--- PagoElectronico v2/PagoElectronico/Login/Login.Designer.cs
+++ PagoElectronico v2/PagoElectronico/Login/Login.Designer.cs
@@ -87,7 +87,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(171, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "admin1";
+            this.textBox1.Text = "admin";
             // 
             // textBox2
             // 
